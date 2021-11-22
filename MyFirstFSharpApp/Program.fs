f083dd115aae0ab39c3b6312046c83e169cd9c70
David Stahl <david.stahl97@outlook.com>
2021-11-22 12:03:54 +0100

    MyFirstFSharpAPp

---
+++ MyFirstFSharpApp/Program.fs
@@ -0,0 +1,9 @@
+﻿
+[<EntryPoint>]
+let main args =
+    // For more information see https://aka.ms/fsharp-console-apps
+    let count = args.Length
+    
+    printfn "Passed in %d items: %A" count args
+
+    0
---
+++ MyFirstFSharpApp/scratchpad.fsx
@@ -0,0 +1,7 @@
+﻿let text = "Hello World"
+text.Length
+
+let greetPerson name age =
+    sprintf "Hello, %s. You are %d years old"
+
+let greeting = greetPerson "Fred" 25
